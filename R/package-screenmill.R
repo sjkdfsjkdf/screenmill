ddb42da3269ec71f98af64266260b9553a3abc2a
Eric Bryant <eeb2139@columbia.edu>
2016-02-26 18:44:16 -0500

    Add package documentation

---
+++ R/package-screenmill.R
@@ -0,0 +1,17 @@
+# ---- Package Documentation ----
+#' Screenmill R package
+#'
+#' Screenmill R package
+#'
+#' @author
+#' Eric Bryant \email{eeb2139@@columbia.edu},
+#'
+#' Maintainer: Eric Bryant \email{eeb2139@@columbia.edu}
+#' @name screenmill
+#' @docType package
+#'
+#' @import dplyr
+#' @import assertthat
+#' @import shiny
+#' @import miniUI
+NULL
