78be360c28026743bf056d1a22332bfb25241c87
Eric Bryant <ericedwardbryant@gmail.com>
2016-03-27 19:44:06 -0400

    Pad numeric names for plate crops to maintain order

--- R/cm-crop.R
+++ R/cm-crop.R
@@ -6,13 +6,22 @@
 #' @param screenmill_plates Path to a screenmill plates annotation CSV file
 #' (the result of \link{annotate_plates}).
 #'
+#' @importFrom stringr str_pad
 #' @export
 
-#library(readr)
-
 crop <- function(screenmill_plates = 'test-scans/screenmill-plates.csv') {
-  data <- read_csv(screenmill_plates)
-  data
+  dir  <- dirname(screenmill_plates)
+
+  data <-
+    readr::read_csv(screenmill_plates) %>%
+    mutate(
+      group3     = stringr::str_pad(group,     width = 3, side = 'left', pad = 0),
+      position3  = stringr::str_pad(position,  width = 3, side = 'left', pad = 0),
+      timepoint3 = stringr::str_pad(timepoint, width = 3, side = 'left', pad = 0)
+    )
+
+  files <- unique(data$file)
+  plate_names <- with(data, paste(date, group3, position3, timepoint3, sep = '-'))
 }
 
 #result <- crop()
