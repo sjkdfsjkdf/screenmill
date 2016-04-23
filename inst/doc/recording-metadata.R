ae296e3d56f7e9dec51f3f2055ee1228255b7e95
Eric Bryant <ericedwardbryant@gmail.com>
2016-04-23 11:09:26 -0400

    Remove old pre-build vignettes

--- inst/doc/recording-metadata.R
+++
@@ -1,6 +0,0 @@
-## ----eval = FALSE--------------------------------------------------------
-#  # Write `plates.csv` and `screens.csv` to the same directory as `cm.txt`
-#  screenmill::new_batch_metadata('path/to/cm.txt')
-#  # If necessary, create a new strain collection keyfile
-#  # screenmill::new_strain_collection(id = 'collection_id', nplates = 2, density = 96)
-
